0428bfe99dbc0b40f2f3016322c57ead2a1dfc46
Pankaj Sonani <pankajsonani@Pankajs-MacBook-Pro.local>
2022-09-06 16:12:20 -0500

    initial commit

---
+++ src/main/java/com/pankaj/cmdline/CmdlineApplication.java
@@ -0,0 +1,38 @@
+package com.pankaj.cmdline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CmdlineApplication implements ApplicationRunner {
+	private static final Logger logger = LoggerFactory.getLogger(CmdlineApplication.class);
+
+	public static void main(String[] args0) {
+		SpringApplication.run(CmdlineApplication.class, args0);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.info("Command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
+		logger.info("Non Option Args: {}", args.getNonOptionArgs());
+		logger.info("Option Names: {}", args.getOptionNames());
+
+		for (String name : args.getOptionNames()){
+			logger.info("arg-" + name + "=" + args.getOptionValues(name));
+		}
+	}
+
+	// run with java -jar cmdline-0.0.1-SNAPSHOT.jar sai pankaj  - this is non arg
+	// java -jar cmdline-0.0.1-SNAPSHOT.jar ram --cmd.prop.1="pankaj" --cmd.prop.2="sai"
+
+
+}
+
+
+
---
+++ src/test/java/com/pankaj/cmdline/CmdlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.cmdline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
